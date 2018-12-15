c3890cff12d354555b6b2b53f1583a3d9a4f97a1
vvvSmile <xsx0813@126.com>
2018-12-16 00:26:37 +0800

    14,15,16 springboot自动配置原理

--- MyMevanProj/src/main/java/cn/smile/springboot/SpringbootApp.java
+++ MyMevanProj/src/main/java/cn/smile/springboot/SpringbootApp.java
@@ -7,6 +7,7 @@ package cn.smile.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+//import org.springframework.boot.autoconfigure.web.servlet.HttpEncodingAutoConfiguration;
 
 @ImportResource(locations={"classpath:beans.xml"})
 @SpringBootApplication
