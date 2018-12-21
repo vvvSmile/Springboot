686b5a8be354349b0152e623a06535aebc75d64f
vvvSmile <xsx0813@126.com>
2018-12-21 23:27:29 +0800

    33 springboot自动配置JdbcTemplate

--- MyMevanProj/src/test/java/cn/smile/springboot/SpringbootAppTest.java
+++ MyMevanProj/src/test/java/cn/smile/springboot/SpringbootAppTest.java
@@ -8,10 +8,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
 import javax.xml.bind.SchemaOutputResolver;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -33,6 +36,11 @@ public class SpringbootAppTest {
     @Autowired
     DataSource dataSource;
 
+
+    //注入JdbcTemplate
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     public void contextLoads(){
         System.out.println(person);
@@ -46,6 +54,12 @@ public class SpringbootAppTest {
 
         System.out.println(dataSource);  //测试DruidDataSource
 
+        //jdbcTemplate应用
+        //org.springframework.jdbc.CannotGetJdbcConnectionException: Failed to obtain JDBC Connection; nested exception is com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException: Unknown database 'boot_demo'
+        /*
+        List<Map<String,Object>> maps=jdbcTemplate.queryForList("SELECT 1 FROM DUAL");
+        System.out.println(maps);  //测试jdbcTemplate
+        */
     }
 
 
