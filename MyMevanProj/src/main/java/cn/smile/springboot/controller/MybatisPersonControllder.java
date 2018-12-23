ca3dab1a79a975478496652b989d7c4e16805b6e
vvvSmile <xsx0813@126.com>
2018-12-23 16:43:00 +0800

    39 springboot开发企业信息系统修改删除

--- MyMevanProj/src/main/java/cn/smile/springboot/controller/MybatisPersonControllder.java
+++ MyMevanProj/src/main/java/cn/smile/springboot/controller/MybatisPersonControllder.java
@@ -24,10 +24,30 @@ public class MybatisPersonControllder {
         return "main";  //返回main页面
     }
 
+
+    @GetMapping("/getPerson")
+    public String main(int pid, Model model){  //添加Model将数据返回给页面,然后在main页面展示，这里main页面以table方式展示
+         MybatisPerson person = personService.getPersonById(pid);
+        model.addAttribute("p",person);
+        return "update";  //跳转到update页面
+    }
+
     @PostMapping("/save")  //error (type=Method Not Allowed, status=405).得用@PostMapping
     public String save(MybatisPerson person){
         personService.insert(person);
         return "redirect:main";  //添加完user之后跳转到main页面
+    }
+
+    @PostMapping("/update")  //error (type=Method Not Allowed, status=405).得用@PostMapping
+    public String update(MybatisPerson person){
+        personService.updatePerson(person);
+        return "redirect:main";  //update完user之后跳转到main页面
+    }
+
+    @GetMapping("/delete")
+    public String delete(int pid){
+        personService.delete(pid);
+        return "redirect:main";  //delete完user之后跳转到main页面
     }
 
 }
